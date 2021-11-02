8f85beb7c83ee3b6f1895325c3d0a2ec52d3a7c1
Trammell Hudson <hudson@trmm.net>
2021-11-02 18:38:30 +0100

    removed math since it is now in the readme

--- library/p5.projection.js
+++ library/p5.projection.js
@@ -1,41 +1,5 @@
 /*
- * Compute the transform matrix to map four input canvas points in xy space
- * (which are normally cartesian, but don't have to be) into
- * four output screen points in uv space (which are typically a
- * skewed quadralateral and gathered by clicking on screen).
- *
- * For a normal perspective transform matrix C from (x,y) to (u,v),
- * the expanded computation is:
- *
- * zi =  c20*xi + c21*yi + 1
- * ui = (c00*xi + c01*yi + c02) / zi
- * vi = (c10*xi + c11*yi + c12) / zi
- *
- * This can be rearranged into:
- *
- * ui * zi = c00*xi + c01*yi + c02
- * ui * (c20*xi + c21*yi + 1) = c00*xi + c01*yi + c02
- * ui * c20 * xi + ui*c21*yi + ui = c00*xi + c01*yi + c02
- * ui = c00*xi + c01*yi + c02 - c20*(ui*xi) - c21*(ui*yi)
- *
- * and 
- *
- * vi * zi = (c10*xi + c11*yi + c12) / zi
- * vi * (c20*xi + c21*yi + 1) = c10*xi + c11*yi + c12
- * vi = c10*xi + c11*yi + c12 - c20*(vi*xi) - c21*(vi*yi)
- *
- * These eight equations can be rearranged into a linear system
- * that allows the coeffecients of the transformat matrix C to
- * be found.
- *
- * / x0 y0  1  0  0  0 -x0*u0 -y0*u0 \ /c00\ /u0\
- * | x1 y1  1  0  0  0 -x1*u1 -y1*u1 | |c01| |u1|
- * | x2 y2  1  0  0  0 -x2*u2 -y2*u2 | |c02| |u2|
- * | x3 y3  1  0  0  0 -x3*u3 -y3*u3 |.|c10|=|u3|
- * |  0  0  0 x0 y0  1 -x0*v0 -y0*v0 | |c11| |v0|
- * |  0  0  0 x1 y1  1 -x1*v1 -y1*v1 | |c12| |v1|
- * |  0  0  0 x2 y2  1 -x2*v2 -y2*v2 | |c20| |v2|
- * \  0  0  0 x3 y3  1 -x3*v3 -y3*v3 / \c21/ \v3/
+ * See https://github.com/osresearch/p5.projection for the math!
  *
  * Inspired by OpenCV perspectiveTransform()
  * https://docs.opencv.org/3.4/d2/de8/group__core__array.html#gad327659ac03e5fd6894b90025e6900a7
@@ -72,20 +36,11 @@ function projectionMatrix(inPts, outPts)
 		[0, 0, 0, x2, y2, 1, -x2*v2, -y2*v2],
 		[0, 0, 0, x3, y3, 1, -x3*v3, -y3*v3],
 	];
+
 	const b = [u0, u1, u2, u3, v0, v1, v2, v3];
+
 	let m = math.lusolve(U, b).map((x) => x[0]);
 
-/*
- * return is c00, c01, c02, c10, c11, c12, c20, c21
- *
- * where:
- *
- * ui = c00*xi + c01*yi + c02
- * vi = c10*xi + c11*yi + c12
- * zi = c20*xi + c21*yi + c22
- *
- * note that c22 is always 1
- */
 	m.push(1); // c22 = 1
 
 	return m;
